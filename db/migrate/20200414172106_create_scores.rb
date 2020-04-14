ac88ea1a7700fedf06f8b322ac38e0a7c1a37cba
etgreer91 <etgreer91@gmail.com>
2020-04-14 11:38:55 -0600

    initializes models and controllers

--- db/migrate/20200414172106_create_scores.rb
+++ db/migrate/20200414172106_create_scores.rb
@@ -1,7 +1,8 @@
 class CreateScores < ActiveRecord::Migration[6.0]
   def change
     create_table :scores do |t|
-      t.integer :score 
+      t.integer :score
+      t.references :user, null: false, foreign_key: true 
 
       t.timestamps
     end
