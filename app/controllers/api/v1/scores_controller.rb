daed0fd3e1421a569377c8b6fdcb409a5def86a9
etgreer91 <etgreer91@gmail.com>
2020-04-15 09:52:42 -0600

    sets up user authentication

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::ScoresController < ApplicationController
+    before_action :authenticate, only: [:index, :create]
+
+    def index
+        @scores = Score.all
+        render json: @scores
+    end
+
+    def create
+        @score = Score.create(
+            user_id: params[:user_id],
+            score: params[:score]
+        )
+
+        render json: @score
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create]
+      resources :scores, only: [:index, :create]
+      post "login", to: "authentication#login"
     end
   end
 end
